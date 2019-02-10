da8000795dafba65e0bf17b6d2657c49c28c2163
Adrien Fallou <adrien.fallou@gmail.com>
2019-02-10 23:37:42 +0100

    Add CLI help text

--- discord_activity/extract/main.py
+++ discord_activity/extract/main.py
@@ -8,14 +8,14 @@ from discord_activity.app import create_app
 def run_extract(graph, destination, before, after):
     if before is None:
         before = now_utc_timestamp()
-    graph.activity_extractor(destination, int(before), int(after))
+    graph.activity_extractor(destination, 1000 * int(before), 1000 * int(after))
 
 def now_utc_timestamp():
-    return 1000 * int(dt.timestamp(dt.utcnow()))
+    return int(dt.timestamp(dt.utcnow()))
 
 @command()
-@option("--before", "-b")
-@option("--after", "-a", required=True)
+@option("--before", "-b", help="Late time boundary for discord messages - as a Unix seconds timestamp")
+@option("--after", "-a", required=True, help="Early time boundary for discord messages - as a Unix seconds timestamp")
 @argument("destination")
 def main(before, after, destination):
     graph = create_app()
